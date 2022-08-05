533b437b5bbbe76af08e46a4b590f49f63658c69
Tsubasa Ishihara <tsubasa.git@gmail.com>
2022-08-05 14:33:54 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tsubasa Ishihara on 2022/08/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
