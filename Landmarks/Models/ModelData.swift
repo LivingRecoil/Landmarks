fa1bfb3451348393fc0ebc462a5815393c901e55
Tsubasa Ishihara <tsubasa.git@gmail.com>
2022-09-25 04:06:52 +0900

    Modify the model data to offer information to Views

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -12,6 +12,13 @@ final class ModelData: ObservableObject {
     // Observing the loaded data
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
